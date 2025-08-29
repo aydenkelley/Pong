685ee99be6da0f08129652336201287ba4a412e8
aydenkelley <aydenfkelley@gmail.com>
2025-08-29 12:12:53 -0700

    actually saved the scene this time, added ball script

---
+++ Assets/Scenes/Ball.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+	float y = Random.Range(0, 2) == 0 ? -1 : 1;
+	rb.linearVelocity = new Vector2(speed * x, speed * y);
+    }
+}
